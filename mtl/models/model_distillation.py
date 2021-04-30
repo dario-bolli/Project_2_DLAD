b4abb1956444e2b406341fbeb6b21b404ee06838
dbolli <dbolli@student.ethz.ch>
2021-04-30 13:20:56 +0000

    Replace model_distillation.py

--- mtl/models/model_distillation.py
+++ mtl/models/model_distillation.py
@@ -44,50 +44,40 @@ class ModelDistillation(torch.nn.Module):
         features = self.encoder(x)
 
         # Uncomment to see the scales of feature pyramid with their respective number of channels.
-        print(", ".join([f"{k}:{v.shape[1]}" for k, v in features.items()]))
+        #print(", ".join([f"{k}:{v.shape[1]}" for k, v in features.items()]))
         lowest_scale = max(features.keys())
 
         features_lowest = features[lowest_scale]
 
         #Semantic Segmentation
-        features_tasks_semseg = self.aspp_semseg(features_lowest)
-        predictions_4x_semseg,features_dec1 = self.decoder_semseg(features_tasks_semseg, features[4])
-
+        features_task_semseg = self.aspp_semseg(features_lowest)
+        predictions_4x_semseg,features_dec1 = self.decoder_semseg(features_task_semseg, features[4])
         predictions_1x_semseg = F.interpolate(predictions_4x_semseg, size=input_resolution, mode='bilinear', align_corners=False)
 
-        print("features_dec1: ", features_dec1.shape)
         #Depth estimation
-        features_tasks_depth = self.aspp_depth(features_lowest)
-        predictions_4x_depth,features_dec2 = self.decoder_depth(features_tasks_depth, features[4])
-
+        features_task_depth = self.aspp_depth(features_lowest)
+        predictions_4x_depth,features_dec2 = self.decoder_depth(features_task_depth, features[4])
         predictions_1x_depth = F.interpolate(predictions_4x_depth, size=input_resolution, mode='bilinear', align_corners=False)
-        print("features_dec2: ", features_dec2.shape)
+
         #Self-Attention
         features_attention_semseg = self.attention_semseg(features_dec1)
         features_attention_depth = self.attention_depth(features_dec2)
-
-        print("features_attention_semseg shape: ", features_attention_semseg.shape)
-        print("features_attention_depth: ", features_attention_depth.shape)
-        print("predictions_1x_semseg: ", predictions_1x_semseg.shape)
-        print("predictions_1x_depth: ", predictions_1x_depth.shape)
         
         #Decoder 3 and 4
         out_semseg_4x,_ = self.decoder3(features_attention_depth, features_dec1)
         out_semseg_1x = F.interpolate(out_semseg_4x, size=input_resolution, mode='bilinear', align_corners=False)
         out_depth_4x,_ = self.decoder4(features_attention_semseg, features_dec2)
         out_depth_1x = F.interpolate(out_depth_4x, size=input_resolution, mode='bilinear', align_corners=False)
-        print("out_semseg: ", out_semseg_1x.shape)
-        print("out_depth: ", out_depth_1x.shape)
 
         out = {}
         offset = 0
 
         for task, num_ch in self.outputs_desc.items():
             if task == 'semseg':
-                out[task] = out_semseg_1x[:, offset:offset+num_ch, :, :]
+                out[task] = [out_semseg_1x[:, offset:offset+num_ch, :, :],predictions_1x_semseg[:, offset:offset+num_ch, :, :]]
                 offset += num_ch
             elif task == 'depth':
-                out[task] = out_depth_1x[:,:, :, :]
+                out[task] = [out_depth_1x[:,:, :, :],predictions_1x_depth[:,:, :, :]]
                 offset += num_ch
 
             else:
